e950e24b19be67a366cedc4d7f91dc24316336ef
Santiago Río <santiago.riosv@gmail.com>
2019-08-06 18:13:03 -0500

    Se integra la webcam para que tome video

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs-extra');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+
+
+// Initialiation
+const app = express();
+
+// Settings
+const port = process.env.port || 3000;
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/models'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, 'public/uploads'),
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + path.extname(file.originalname));
+  }
+});
+app.use(multer({ storage }).single('image'));
+
+
+// Routes
+app.use('/', require('./routes/index.routes'));
+
+
+app.listen(port, () => {
+  console.log(`Server online on port ${port}`);
+})
---
+++ routes/index.routes.js
@@ -0,0 +1,48 @@
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const fs = require('fs');
+const { Router } = require('express');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'sriosv',
+    api_key: '551141971617259',
+    api_secret: 'bqnhXJ6RZ5PEz8v3HlygI7TdcKA'
+})
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('face_detection');
+});
+
+router.post('/add', async (req, res) => {
+
+    console.log("Imagen ", req.file);
+    // const result = await cloudinary.v2.uploader.upload(req.file.path);
+    console.log(result.url);
+
+    const visualRecognition = new VisualRecognitionV3({
+        version: '2018-03-19',
+        iam_apikey: '3DeN5GN0szXkNUbZRP86uz6HD8ymsYJbYYhXB7EZa4jN',
+    })
+
+    var url = result.secure_url;
+
+    var params = {
+        url: url,
+    };
+
+    visualRecognition.classify(params, function (err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(JSON.stringify(response, null, 2))
+        }
+    });
+
+    res.render('image_form')
+    await fs.unlink(req.file.path, (err) => {
+        if (err) throw err;
+        console.log('Archivo eliminado');
+    })
+})
+
+module.exports = router;
